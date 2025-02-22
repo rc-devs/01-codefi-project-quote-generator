2f84670c735bb00b8596b3c4ea6a65074341f2bd
rc.devs9 <rc.devs9@gmail.com>
2025-02-22 16:38:50 -0600

    parse JSON string for array, innerHTML display

--- scripts/manage-affirmations.js
+++ scripts/manage-affirmations.js
@@ -1,17 +1,32 @@
 console.log('connected to manage-affirmations.js');
-
 //imports
 const retrievedData = localStorage.getItem("affirmationsJSON")
 
 //dom communication
-const manageContainer = document.querySelector('#manage-affirmations-container');
+const manageContainer = document.querySelector('#js-manage-affirmations-container');
+const testElem = document.querySelector('#js-test-element');
 
 //event listeners
 
 
 
 function displayAffirmationsArray() {
-  manageContainer.innerHTML = retrievedData
-  
+  let parsedData = JSON.parse(retrievedData) //destring JSON data
+  let index = parsedData[0];
+
+  parsedData.forEach((quote) => {
+    manageContainer.innerHTML += 
+    `
+    <div id="js-quote-container">
+    <p>${quote}</p>
+      <button class="js-delete">Delete</button>
+      <button class="js-favorite">Add to Favorites</button>
+    </div>
+    `;
+ 
+  });
+
 };
-displayAffirmationsArray();
+displayAffirmationsArray(); //call on load to display list
+
+

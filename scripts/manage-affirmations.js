a85e7bec418de043b4e0fef24b1cb3d7228fb39a
rc.devs9 <rc.devs9@gmail.com>
2025-02-22 17:51:13 -0600

    add dom/event listener, start delete function, naming conventions

--- scripts/manage-affirmations.js
+++ scripts/manage-affirmations.js
@@ -4,17 +4,15 @@ const retrievedData = localStorage.getItem("affirmationsJSON")
 
 //dom communication
 const manageContainer = document.querySelector('#js-manage-affirmations-container');
-const testElem = document.querySelector('#js-test-element');
 
-//event listeners
+//function runs on load
+displayAffirmationsArray(); //call on load to display list
 
 
+function displayAffirmationsArray() { //display data
+  let affirmationsArray = JSON.parse(retrievedData) //destring JSON data
 
-function displayAffirmationsArray() {
-  let parsedData = JSON.parse(retrievedData) //destring JSON data
-  let index = parsedData[0];
-
-  parsedData.forEach((quote) => {
+  affirmationsArray.forEach((quote) => { //loop through array, display each value (quote) in html
     manageContainer.innerHTML += 
     `
     <div id="js-quote-container">
@@ -23,10 +21,23 @@ function displayAffirmationsArray() {
       <button class="js-favorite">Add to Favorites</button>
     </div>
     `;
- 
+
+    //new dom and event listener
+    const btnDelete = document.querySelectorAll('.js-delete'); //delete dom
+    btnDelete.forEach((btn) => {btn.addEventListener('click', handleDelete)}); 
+
   });
-
 };
-displayAffirmationsArray(); //call on load to display list
 
+
+function handleDelete(){
+  let affirmationsArray = JSON.parse(retrievedData) //destring JSON data
+
+  affirmationsArray.splice(0, 1);
+  
+
+  console.log('delete');
+  console.log(retrievedData);
+  console.log(affirmationsArray);
+  };
 

8bb8430c2142a7df355a69b10ac6cf63458bc4db
rc.devs9 <rc.devs9@gmail.com>
2025-02-22 18:47:41 -0600

    trying accumulator pattern for CRUD

--- scripts/manage-affirmations.js
+++ scripts/manage-affirmations.js
@@ -11,8 +11,31 @@ displayAffirmationsArray(); //call on load to display list
 
 function displayAffirmationsArray() { //display data
   let affirmationsArray = JSON.parse(retrievedData) //destring JSON data
+  let displayHTML = '';
 
-  affirmationsArray.forEach((quote) => { //loop through array, display each value (quote) in html
+  for (let i = 0; i < affirmationsArray.length; i++) {
+    const quote = affirmationsArray[i];
+    const display = 
+      `
+      <div id="js-quote-container">
+      <p>${quote}</p>
+        <button class="js-delete" onclick=
+        "
+        affirmationsArray.splice(${i}, 1);
+        displayAffirmationsArray()
+        "
+        >Delete</button>
+        <button class="js-favorite">Add to Favorites</button>
+      </div>
+      `;
+    displayHTML += display;
+  }
+
+  document.querySelector('#js-manage-affirmations-container').innerHTML = displayHTML;
+
+  
+
+/*   affirmationsArray.forEach((quote) => { //loop through array, display each value (quote) in html
     manageContainer.innerHTML += 
     `
     <div id="js-quote-container">
@@ -33,11 +56,13 @@ function displayAffirmationsArray() { //display data
 function handleDelete(){
   let affirmationsArray = JSON.parse(retrievedData) //destring JSON data
 
-  affirmationsArray.splice(0, 1);
+  for (let i = 0; i < affirmationsArray.length; i++) 
+  affirmationsArray.splice(i, 1);
+  affirmationsArray.
+  displayAffirmationsArray();
   
-
   console.log('delete');
   console.log(retrievedData);
-  console.log(affirmationsArray);
+  console.log(affirmationsArray); */
   };
 

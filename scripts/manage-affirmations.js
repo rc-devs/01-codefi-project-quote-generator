622059a4e4e734a425a879284a96e193d0e9c195
rc.devs9 <rc.devs9@gmail.com>
2025-02-24 14:12:50 -0600

    splice from affirmations array (not displayed in html yet)

--- scripts/manage-affirmations.js
+++ scripts/manage-affirmations.js
@@ -19,13 +19,9 @@ function displayAffirmationsArray() { //display data
       `
       <div id="js-quote-container">
       <p>${quote}</p>
-        <button class="js-delete" onclick=
-        "
-        affirmationsArray.splice(${i}, 1);
-        displayAffirmationsArray()
-        "
+        <button data-index="${i}" class="js-delete" 
         >Delete</button>
-        <button class="js-favorite">Add to Favorites</button>
+        <button  class="js-favorite">Add to Favorites</button>
       </div>
       `;
     displayHTML += display;
@@ -33,22 +29,48 @@ function displayAffirmationsArray() { //display data
 
   document.querySelector('#js-manage-affirmations-container').innerHTML = displayHTML;
 
- /*  //new dom and event listener created
+  //new dom and event listener created for delete
   const btnDelete = document.querySelectorAll('.js-delete'); //delete dom
-  btnDelete.forEach((btn) => {btn.addEventListener('click', handleDelete)});  */
+  btnDelete.forEach((btn) => {
+    btn.addEventListener('click', () => {
+    let currentBtnIndex = btn.dataset.index;
+    let affirmationsArray = JSON.parse(retrievedData) //destring JSON data
+   
+    console.log("this is the object array",affirmationsArray); //test array
+  
+  affirmationsArray.splice(currentBtnIndex, 1)
+    console.log(affirmationsArray);
+    
+
+    //affirmationsArray.displayAffirmationsArray();
+    
+    
+    // console.log('delete');
+    // console.log("this is the JSON String", retrievedData); 
+    // console.log("this is the object array",affirmationsArray);
+    }
+  )})
 };
 
-/* 
-function handleDelete(){
-  let affirmationsArray = JSON.parse(retrievedData) //destring JSON data
 
-  for (let i = 0; i < affirmationsArray.length; i++) 
-  affirmationsArray.splice(i, 1);
-  affirmationsArray.
-  displayAffirmationsArray();
+// function handleDelete(btn){
+//   const currentBtnIndex = btn.dataset.index;
+//   console.log(currentBtnIndex);
+//   const currentIndex = null;
+//   let affirmationsArray = JSON.parse(retrievedData) //destring JSON data
+ 
+//   console.log("this is the object array",affirmationsArray); //test array
+
+//   for (let i = 0; i < affirmationsArray.length; i++) {
+//     console.log(affirmationsArray[i])
+//     //affirmationsArray.splice(i, 1);
+//   }
   
-  console.log('delete');
-  console.log(retrievedData);
-  console.log(affirmationsArray);
-  }; */
+//   //affirmationsArray.displayAffirmationsArray();
+  
+  
+//   console.log('delete');
+//   console.log("this is the JSON String", retrievedData); 
+//   console.log("this is the object array",affirmationsArray);
+//   };
 

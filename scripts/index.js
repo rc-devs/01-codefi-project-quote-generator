e35998755a90f8d35dabadb577839eee817eb599
rc.devs9 <rc.devs9@gmail.com>
2025-03-02 11:27:06 -0600

    move all to one js and html to avoid storage issues

--- scripts/index.js
+++ scripts/index.js
@@ -15,16 +15,18 @@ let retrieveAffirmationsJSON = sessionStorage.getItem("affirmationsJSON");
 console.log(retrieveAffirmationsJSON)
 //communicate with document elements
 const btnGenerateAffirmation = document.querySelector('#js-generate-affirmation');
-const btnResetAffirmation = document.querySelector('#js-reset-affirmation');
 const displayQuote = document.querySelector('#js-display-quote');
 const inputAffirmation = document.querySelector('#js-input-affirmation');
 const btnAddAffirmation = document.querySelector('#js-add-affirmation-button');
+const btnManageAffirmations = document.querySelector('#js-manage-button')
+const manageContainer = document.querySelector('#manage-container')
+
 
 
 //event listeners
 btnGenerateAffirmation.addEventListener('click', handleGenerate);
-btnResetAffirmation.addEventListener('click', handleReset);
 btnAddAffirmation.addEventListener('click', handleAdd);
+btnManageAffirmations.addEventListener('click', displayList);
 
 
 
@@ -40,8 +42,8 @@ function updateStorage(){
   
   if (affirmationsArray == "") { //check if array empty
     alert('You have reached the end of your affirmations list. Your list has been reset.')
+    affirmationsArray = JSON.parse(affirmationsJSON)
     handleReset();
-    
     console.log('js object', affirmationsArray) //test log
     console.log('json storage', affirmationsJSON); //test log to see if array was reset to original
   } else {
@@ -49,13 +51,12 @@ function updateStorage(){
     affirmationsArray.splice(idRandomIndex, 1) //drop index from array (no repeats)
    
     displayQuote.textContent = `${randomAffirmation}` //display value of random index in html
+    //affirmationsArray = JSON.parse(affirmationsJSON)
     console.log('js object', affirmationsArray) //test log
     console.log('json storage', affirmationsJSON); //test log to see if array was reset to original
   }
 };
  
-
-//handleReset
 function handleReset(){
   affirmationsArray = JSON.parse(affirmationsJSON); //retreive array from storage and reassign to variable
   
@@ -65,8 +66,6 @@ function handleReset(){
   console.log('json storage', affirmationsJSON); //test log to see if array was reset to original
 }
 
-
-//add quote
 function handleAdd(){
   console.log('handleAdd runs');
   affirmationsArray.push(inputAffirmation.value); //push input value to array
@@ -78,7 +77,56 @@ function handleAdd(){
 
   console.log('js object', affirmationsArray); //test log to ensure input was pushed to array
   console.log('json storage', affirmationsJSON); //test log to ensure local storage update
-  console.log(retrieveAffirmationsJSON)
+  displayList(affirmationsJSON);
 };
 
 
+function displayList() { //display data
+  let displayedArray = JSON.parse(affirmationsJSON) //destring JSON data
+  let displayHTML = '';
+  console.log(displayedArray)
+
+  for (let i = 0; i < displayedArray.length; i++) {
+    const quote = displayedArray[i];
+    const display = 
+      `
+      <div id="js-quote-container">
+      <p>${quote}</p>
+        <button data-index="${i}" class="js-delete" 
+        >Delete</button>
+        <button  class="js-favorite">Add to Favorites</button>
+      </div>
+      `;
+    displayHTML += display;
+  }
+  document.querySelector('#manage-container').innerHTML = displayHTML;
+  console.log('displayAffrimationsArray runs')
+
+  //new dom delete
+  const btnDelete = document.querySelectorAll('.js-delete');
+    
+  //delete (?)function event
+  btnDelete.forEach((btn) => {
+    btn.addEventListener('click', () => {
+    let currentBtnIndex = btn.dataset.index;
+    let displayedArray = JSON.parse(affirmationsJSON) //destring JSON data
+    
+    //test log
+    console.log("this is the affrimations array", displayedArray); //current array
+
+    //delete from array
+    displayedArray.splice(currentBtnIndex, 1); 
+
+    //local storage
+    affirmationsJSON = JSON.stringify(displayedArray); //change js back to JSON
+    sessionStorage.setItem("affirmationsJSON", affirmationsJSON);
+    affirmationsArray = JSON.parse(affirmationsJSON)
+    //test logs
+    console.log("affirmations array after delete", displayedArray); //array post delete
+    console.log('delete'); //delete confirm
+
+    //change innerhtml
+    displayList(); 
+    });
+  });
+};
--- scripts/manage-affirmations.js
+++ scripts/manage-affirmations.js
@@ -11,19 +11,26 @@ console.log(affirmationsJSON) */
 //dom communication
 const manageContainer = document.querySelector('#js-manage-affirmations-container');
 const home = document.querySelector(".homeAnchor");
+const inputAffirmation = document.querySelector('#js-input-affirmation');
+const btnAddAffirmation = document.querySelector('#js-add-affirmation-button')
+const displayTextarea = document.querySelector("#display-textarea")
+
+
 
 //event listeners
 //home.addEventListener('click', updateStorage)
+btnAddAffirmation.addEventListener('click', handleAdd)
+
 
 //function runs on load
 displayAffirmationsArray(); //call on load to display list
 function displayAffirmationsArray() { //display data
-  let affirmationsArray = JSON.parse(affirmationsJSON) //destring JSON data
+  let displayedArray = JSON.parse(affirmationsJSON) //destring JSON data
   let displayHTML = '';
-  console.log(affirmationsArray)
+  console.log(displayedArray)
 
-  for (let i = 0; i < affirmationsArray.length; i++) {
-    const quote = affirmationsArray[i];
+  for (let i = 0; i < displayedArray.length; i++) {
+    const quote = displayedArray[i];
     const display = 
       `
       <div id="js-quote-container">
@@ -45,20 +52,20 @@ function displayAffirmationsArray() { //display data
   btnDelete.forEach((btn) => {
     btn.addEventListener('click', () => {
     let currentBtnIndex = btn.dataset.index;
-    let affirmationsArray = JSON.parse(affirmationsJSON) //destring JSON data
+    let displayedArray = JSON.parse(affirmationsJSON) //destring JSON data
     
     //test log
-    console.log("this is the affrimations array", affirmationsArray); //current array
+    console.log("this is the affrimations array", displayedArray); //current array
 
     //delete from array
-    affirmationsArray.splice(currentBtnIndex, 1); 
+    displayedArray.splice(currentBtnIndex, 1); 
 
     //local storage
-    affirmationsJSON = JSON.stringify(affirmationsArray); //change js back to JSON
+    affirmationsJSON = JSON.stringify(displayedArray); //change js back to JSON
     sessionStorage.setItem("affirmationsJSON", affirmationsJSON);
 
     //test logs
-    console.log("affirmations array after delete", affirmationsArray); //array post delete
+    console.log("affirmations array after delete", displayedArray); //array post delete
     console.log('delete'); //delete confirm
 
     //change innerhtml
@@ -70,6 +77,36 @@ function displayAffirmationsArray() { //display data
 /* 
 //function to update local storage when going home
 function updateStorage(){
-  affirmationsJSON = JSON.stringify(affirmationsArray); //change js back to JSON
+  affirmationsJSON = JSON.stringify(displayedArray); //change js back to JSON
   sessionStorage.setItem("affirmationsJSON", affirmationsJSON);
 } */
+
+
+
+//handleReset
+function handleReset(){
+  affirmationsArray = JSON.parse(affirmationsJSON); //retreive array from storage and reassign to variable
+  
+  console.log('The quotes have been reset.') 
+  displayQuote.textContent = `Inspiration Awaits!` //reset html display
+  console.log('js object', affirmationsArray) //test log
+  console.log('json storage', affirmationsJSON); //test log to see if array was reset to original
+}
+
+
+//add quote
+function handleAdd(){
+  console.log('handleAdd runs');
+  affirmationsArray.push(inputAffirmation.value); //push input value to array
+  affirmationsJSON = JSON.stringify(affirmationsArray); //
+  sessionStorage.setItem("affirmationsJSON", affirmationsJSON);
+
+  alert(`Your quote "${inputAffirmation.value}" has been added to your list of affirmations!`); //notify user of change
+  inputAffirmation.value = ""; //clear html text area for new input
+
+  console.log('js object', affirmationsArray); //test log to ensure input was pushed to array
+  console.log('json storage', affirmationsJSON); //test log to ensure local storage update
+  displayAffirmationsArray(affirmationsJSON);
+
+};
+

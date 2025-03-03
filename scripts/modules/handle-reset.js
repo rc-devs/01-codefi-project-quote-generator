af35d789b5a77a139d7a2170c734bd83d89a868b
rc.devs9 <rc.devs9@gmail.com>
2025-03-03 10:58:48 -0600

    removed attempted export files

--- scripts/modules/handle-reset.js
+++
@@ -1,8 +0,0 @@
-export function handleReset(){
-  affirmationsArray = JSON.parse(affirmationsJSON); //retreive array from storage and reassign to variable
-  
-  console.log('The quotes have been reset.') 
-  displayQuote.textContent = `Inspiration Awaits!` //reset html display
-  console.log('js object', affirmationsArray) //test log
-  console.log('json storage', affirmationsJSON); //test log to see if array was reset to original
-}
